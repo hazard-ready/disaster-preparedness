184682f0ce0d0b80d4adb77ea16fef87bf7cb6f2
Tony Schick <schick.anthony@gmail.com>
2015-03-23 11:01:35 -0700

    changed database settings for heroku

--- cascadiaprepared/settings.py
+++ cascadiaprepared/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = SECURE_SETTINGS_SECRET_KEY
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
