c3a081ce526088366e544e5f369b6321d365e2d4
Tony Schick <schick.anthony@gmail.com>
2015-03-23 14:50:56 -0700

    database config for heroku

--- cascadiaprepared/settings.py
+++ cascadiaprepared/settings.py
@@ -66,11 +66,11 @@ WSGI_APPLICATION = 'cascadiaprepared.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'geodjango' # SECURE_SETTINGS_DB_NAME,
-       #'USER': SECURE_SETTINGS_DB_USER,
-       #'PASSWORD': SECURE_SETTINGS_DB_PASSWORD,
-       #'HOST': SECURE_SETTINGS_DB_HOST,
-       #'PORT': SECURE_SETTINGS_DB_PORT,
+        'NAME': 'geodjango',
+        'USER': '' #SECURE_SETTINGS_DB_USER,
+        'PASSWORD': '' #SECURE_SETTINGS_DB_PASSWORD,
+        'HOST': '' #SECURE_SETTINGS_DB_HOST,
+        'PORT': '' #SECURE_SETTINGS_DB_PORT,
     }
 }
 
@@ -96,9 +96,9 @@ STATIC_URL = '/static/'
 
 ### HEROKU CONFIGURATIONS ###
 # added per instructions at https://devcenter.heroku.com/articles/getting-started-with-django
+# Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
-DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -108,12 +108,15 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+# Specially for GeoDjango on Heroku
 GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
 GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
 
