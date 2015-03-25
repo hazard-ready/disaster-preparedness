89e1c145be3a53263ea6dacf5e03a21c7cb2f0d9
Weston Wedding <weston@sticky.tv>
2015-03-25 16:24:26 -0700

    Bye bye world models, hello better documented data models, and nicer string representations!

--- world/load.py
+++ world/load.py
@@ -1,30 +1,15 @@
 import os
 from django.contrib.gis.utils import LayerMapping
-from .models import WorldBorder
+
 from .models import TsunamiZone
 from .models import ImpactZone
 
-world_mapping = {
-    'fips' : 'FIPS',
-    'iso2' : 'ISO2',
-    'iso3' : 'ISO3',
-    'un' : 'UN',
-    'name' : 'NAME',
-    'area' : 'AREA',
-    'pop2005' : 'POP2005',
-    'region' : 'REGION',
-    'subregion' : 'SUBREGION',
-    'lon' : 'LON',
-    'lat' : 'LAT',
-    'mpoly' : 'MULTIPOLYGON',
-}
-
-# Auto-generated `LayerMapping` dictionary for TsunamiZone model
+# Auto-generated `LayerMapping` dictionary for TsunamiZone model,
+# modified later by cool devs.
 tsunamizone_mapping = {
-    'pid' : 'Id',
-    'name' : 'Location',
+    'scenario_id' : 'Id',
     'location' : 'Location',
-    'type' : 'Type',
+    'scenario_type' : 'Type',
     'geom' : 'MULTIPOLYGON',
 }
 
@@ -38,15 +23,11 @@ impactzone_mapping = {
     'geom' : 'Polygon',
 }
 
-world_shp = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/TM_WORLD_BORDERS-0.3.shp'))
 tsunami_shp = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/DOGAMI_TsunamiEvacuationZones_2013.shp'))
 impact_shp = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/ORP_Impact_zones.shp'))
 
 
 def run(verbose=True):
-    lm = LayerMapping(WorldBorder, world_shp, world_mapping,
-                      transform=False, encoding='iso-8859-1')
-    lm.save(strict=True, verbose=False)
 
     lm2 = LayerMapping(TsunamiZone, tsunami_shp, tsunamizone_mapping,
                       transform=True, encoding='iso-8859-1')
