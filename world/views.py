e3b5fbd6823f3cfe2de2e5849aa7901f3b2124f8
Weston Wedding <weston@sticky.tv>
2015-03-25 16:34:37 -0700

    Forgot to remove the world borders from views.

--- world/views.py
+++ world/views.py
@@ -8,10 +8,9 @@ def zoneCheck(request):
     lat = -123.9125932
     lng = 45.9928274
     pnt = Point(lat, lng)    
-    qs_w = WorldBorder.objects.filter(mpoly__contains=pnt);
     qs_t = TsunamiZone.objects.filter(geom__contains=pnt);
     qs_i = ImpactZone.objects.filter(geom__contains=pnt);
 
-    zonesStrings = list(chain(qs_w, qs_t, qs_i));
+    zonesStrings = list(chain(qs_t, qs_i));
     context = {'lat': lat, 'lng': lng, 'areas': zonesStrings}
     return render(request, 'zonecheck.html', context)
