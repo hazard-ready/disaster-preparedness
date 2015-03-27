0273dcfd5a3e34f3f4015da014d92247afd241b6
Weston Wedding <weston@sticky.tv>
2015-03-26 20:53:13 -0700

    Created a really rough nugget lookup based only on impact zones.  Along with SnuggetCheck!

--- cascadiaprepared/urls.py
+++ cascadiaprepared/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'cascadiaprepared.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    (r'(?i)^zoneCheck', views.zoneCheck),   
+    (r'(?i)^zoneCheck', views.zoneCheck),
+    (r'(?i)^snuggetCheck', views.snuggetCheck),  
     url(r'^admin/', include(admin.site.urls)),
 )
