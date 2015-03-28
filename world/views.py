bcc0c7a94adc41fa4c461752d49915e9b817c81e
Weston Wedding <weston@sticky.tv>
2015-03-27 19:47:40 -0700

    Fixing a stupid bug in the lat/lng getting switched in lookups, and fixing the workaround Brandon is using.

--- world/views.py
+++ world/views.py
@@ -11,7 +11,7 @@ def app_view(request):
 
     if len(lat) > 0:
     
-      snugget_content = Snugget.findSnuggetsForPoint(lat=float(lng), lng=float(lat))    
+      snugget_content = Snugget.findSnuggetsForPoint(lat=float(lat), lng=float(lng))    
         
       return render(request, 'index.html', {'data': snugget_content, 'lat':lat, 'lng':lng})    
 
