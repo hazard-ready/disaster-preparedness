b867ff7aa082e82df09eb95582455897e7967e73
Weston Wedding <weston@sticky.tv>
2015-03-28 15:37:49 -0700

    Trying to revert to the original shape file that got re-generated but that didn't work, either.

---
+++ world/migrations/0039_auto_20150328_2134.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.contrib.gis.db.models.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('world', '0038_impactzonedata'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='impactzonedata',
+            name='geom',
+            field=django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326),
+            preserve_default=True,
+        ),
+    ]
