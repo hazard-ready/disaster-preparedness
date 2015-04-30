68964038d035a578ed93d017bd65f73c0f868ee5
Weston Wedding <weston@sticky.tv>
2015-04-30 16:10:03 -0700

    Updated admin listing screen substantially to support filtering, sorting.  Individual edit forms are also clearer with filters grouped.

--- world/admin.py
+++ world/admin.py
@@ -6,14 +6,45 @@ admin.site.register(TsunamiZone, admin.GeoModelAdmin)
 admin.site.register(ImpactZoneData, admin.GeoModelAdmin)
 admin.site.register(ExpectedGroundShaking, admin.GeoModelAdmin)
 admin.site.register(ImpactZone, admin.GeoModelAdmin)
-admin.site.register(SnuggetType, admin.ModelAdmin)
 admin.site.register(SnuggetSection, admin.ModelAdmin)
 admin.site.register(SnuggetSubSection, admin.ModelAdmin)
-admin.site.register(TextSnugget, admin.ModelAdmin)
 
-class EmbedAdmin(AdminVideoMixin, admin.ModelAdmin):
-    pass
+class SnuggetAdmin(admin.ModelAdmin):
+    list_display = ('shortname', 'shaking_filter', 'impact_zone_filter', 'tsunami_filter', 'liquifaction_filter', 'landslide_filter', 'section', 'sub_section')
+    list_filter = ('shaking_filter', 'impact_zone_filter', 'tsunami_filter', 'liquifaction_filter', 'landslide_filter', 'section', 'sub_section')
+    
+    fieldsets = (
+        (None, {
+           'fields': ('section', 'sub_section'),
+        }),
+        ('Filters', {
+            'description': 'Choose a filter value this snugget will show up for.  It is recommended you only select a value for one filter and leave the rest empty.',
+            'fields': (('shaking_filter', 'impact_zone_filter', 'tsunami_filter', 'liquifaction_filter', 'landslide_filter'),)
+        }),
+    )    
+    
+    def shortname(self, obj):
+        return "Undefined";
 
+class TextAdmin(SnuggetAdmin):
+    fieldsets = SnuggetAdmin.fieldsets + ((None, {
+            'fields': ('content',),
+        }),
+    )
+    
+    def shortname(self, obj):
+        return 'Text: "' + str(obj) + '"';
+    
+class EmbedAdmin(AdminVideoMixin, SnuggetAdmin):    
+    fieldsets = SnuggetAdmin.fieldsets + ((None, {
+            'fields': ('embed',),
+        }),
+    )
+    
+    def shortname(self, obj):
+        return "EmbedSnugget";
+    
+admin.site.register(TextSnugget, TextAdmin)
 admin.site.register(EmbedSnugget, EmbedAdmin)
 
 # Maybe in the future
