d93e56177a1fa7bde16164a3a68686bb1cb1f555
Weston Wedding <weston@sticky.tv>
2015-05-01 10:29:35 -0700

    Indent fix on views.py didn't go quite as planned.

--- world/views.py
+++ world/views.py
@@ -9,10 +9,9 @@ def app_view(request):
         lat = request.GET['lat']
         lng = request.GET['lng']
 
-    if len(lat) > 0:
-        snugget_content = Snugget.findSnuggetsForPoint(lat=float(lat), lng=float(lng))
-
-        return render(request, 'index.html', {'data': snugget_content, 'has_location': True})
+        if len(lat) > 0:
+            snugget_content = Snugget.findSnuggetsForPoint(lat=float(lat), lng=float(lng))
+            return render(request, 'index.html', {'data': snugget_content, 'has_location': True})
 
     # if not, we'll still serve up the same template without data
     else:
