9d464732a9d5dd3b283cd0e6b15ed89703b20e14
Weston Wedding <weston@sticky.tv>
2015-05-04 10:04:34 -0700

    The view does some more organizing in order to make templates cleaner and hopefully easier to build.  Removed individual snugget.section == "Blah" checks all over the place in templates.

--- world/views.py
+++ world/views.py
@@ -10,8 +10,26 @@ def app_view(request):
         lng = request.GET['lng']
 
         if len(lat) > 0:
-            
             snugget_content = Snugget.findSnuggetsForPoint(lat=float(lat), lng=float(lng))
+            snugget_content['structured'] = {
+                'moment': {},
+                'recovery': {},
+                'prepare': {}
+                }
+
+            # Make our lives easier by additionally sorting these snugs into our 3 sections.
+            for groupkey, group in snugget_content['groups'].items():
+                snugget_content['structured']['moment'].setdefault(groupkey, [])
+                snugget_content['structured']['recovery'].setdefault(groupkey, [])
+                snugget_content['structured']['prepare'].setdefault(groupkey, [])
+
+                for snugget in group:
+                    if snugget.section.name == "The Moment":
+                        snugget_content['structured']['moment'][groupkey].append(snugget)
+                    elif snugget.section.name == "Community Recovery":
+                        snugget_content['structured']['recovery'][groupkey].append(snugget)
+                    elif snugget.section.name == "How To Prepare":
+                        snugget_content['structured']['prepare'][groupkey].append(snugget)
             return render(request, 'index.html', {'data': snugget_content, 'has_location': True})
 
     # if not, we'll still serve up the same template without data
