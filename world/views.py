4c520e10c65a4749cc949155ba51e32b3daede30
Weston Wedding <weston@sticky.tv>
2015-05-04 08:48:29 -0700

    Updated the snugget structure to combine both the liquefaction and landslide deformation into a single "deform" array.  Also updated views to iterate through that list. Added a deform level to the data structure that is the max of either liquefaction or landslide.

--- world/views.py
+++ world/views.py
@@ -10,7 +10,8 @@ def app_view(request):
         lng = request.GET['lng']
 
         if len(lat) > 0:
-            snugget_content = Snugget.findSnuggetsForPoint(lat=float(lat), lng=float(lng))            
+            
+            snugget_content = Snugget.findSnuggetsForPoint(lat=float(lat), lng=float(lng))
             return render(request, 'index.html', {'data': snugget_content, 'has_location': True})
 
     # if not, we'll still serve up the same template without data
