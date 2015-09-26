4d3a73396a3f7554bc54ae7c0b48b27779b1f505
Jason <jasonbernert@gmail.com>
2015-09-26 00:43:56 -0700

    settings.py file

---
+++ cascadiaprepared/settings.py
@@ -0,0 +1,103 @@
+"""
+Django settings for cascadiaprepared project.
+"""
+
+ADMINS = (
+          ('Weston Wedding', 'wes@wawco.com')
+         )
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+from os import environ
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+# TODO: Deprecated in Django 1.8, remove when that happens.
+TEMPLATE_DEBUG = False
+
+if DEBUG:
+    ALLOWED_HOSTS = ['opb.org', 'aftershock.local', 'localhost', '127.0.0.1', '0.0.0.0']
+else:
+    ALLOWED_HOSTS = ['opb.org', 'arcane-dawn-5464.herokuapp.com']
+
+# Application definition
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'embed_video',
+    'world'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'cascadiaprepared.urls'
+
+WSGI_APPLICATION = 'cascadiaprepared.wsgi.application'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+### HEROKU CONFIGURATIONS ###
+# Added per instructions at https://devcenter.heroku.com/articles/getting-started-with-django
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+
+# Allow database connections to persist
+CONN_MAX_AGE = environ.get('CONN_MAX_AGE') or 0
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Static asset configuration
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/aftershock/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Specially for GeoDjango on Heroku
+GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
+
+### ^^^^^^^^^^^^^^^^^^^^^^^^^ ###
+### END HEROKU CONFIGURATIONS ###
