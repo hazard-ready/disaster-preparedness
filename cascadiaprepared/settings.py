6d772ee8e974bc4bed6c4c0d47749be661a3b384
Jason Bernert <jasonbernert@gmail.com>
2015-09-28 20:59:16 -0700

    Changed STATIC_URL to public web hosting endpoint

--- cascadiaprepared/settings.py
+++ cascadiaprepared/settings.py
@@ -87,7 +87,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = 'https://s3-us-west-2.amazonaws.com/opb-news-interactives/aftershock/'
+STATIC_URL = 'http://opb-news-interactives.s3-website-us-west-2.amazonaws.com/aftershock/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
