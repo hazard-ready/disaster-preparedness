765450e11854312c7619c123043f4e1bfd8f3904
nein09 <nein09@nein09.com>
2015-12-04 10:33:30 -0800

    Migrations

---
+++ world/migrations/0060_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('world', '0059_auto_20151204_0034'),
+        ('world', '0059_auto_20151202_2347'),
+    ]
+
+    operations = [
+    ]
