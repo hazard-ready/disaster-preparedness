261fc8f4070b81d5428fcba729c2f669f033139b
Eldan Goldenberg <eldang@gmail.com>
2015-12-21 14:33:25 -0800

    Separated reproject and simplify steps
    The tolerance argument for simplify is in units dictated by the
    projection, so we can only get a consistent simplification tolerance if
    we make sure everything’s in the same SRS *first*.

--- import.py
+++ import.py
@@ -3,7 +3,10 @@ import subprocess
 import sys
 
 def main():
-  workingDir = "world/data"
+  dataDir = "world/data"
+  reprojectedDir = os.path.join(dataDir, "reprojected")
+  simplifiedDir = os.path.join(dataDir, "simplified")
+
   modelsClasses = ""
   adminModelImports = "from .models import TextSnugget, EmbedSnugget, SnuggetSection, SnuggetSubSection, Infrastructure, InfrastructureGroup, InfrastructureCategory, RecoveryLevels, Location, SiteSettings"
   adminSiteRegistrations = ""
@@ -16,33 +19,51 @@ def main():
   loadPaths = ""
   templateMomentSnuggets = ""
   loadImports = ""
-  for f in os.listdir(workingDir):
-    if f[-4:] == ".shp" and f[-16:] != "_reprojected.shp":
-      print("Opening shapefile:", f[:-4])
-      reprojected = reprojectShapefile(f, workingDir, "EPSG:4326")
-      
 
-      
-def reprojectShapefile(f, workingDir, srs):
-  reprojected = f[:-4] + "_reprojected.shp"
-  if reprojected in os.listdir(workingDir):
+  for subdir in [reprojectedDir, simplifiedDir]:
+    if not os.path.exists(subdir):
+      os.mkdir(subdir)
+
+  for f in os.listdir(dataDir):
+    if f[-4:] == ".shp":
+      print("Opening shapefile:", f[:-4])
+      reprojected = reprojectShapefile(f, dataDir, reprojectedDir, "EPSG:4326")
+      simplified = simplifyShapefile(reprojected, simplifiedDir, "0.0001")
+
+
+
+def reprojectShapefile(f, inputDir, outputDir, srs):
+  original = os.path.join(inputDir, f)
+  reprojected = os.path.join(outputDir, f)
+  if os.path.exists(reprojected):
     print("Skipping reprojection because this file has previously been reprojected.")
   else:
-    print("Simplifying and reprojecting to", srs)
-    reprojected = f[:-4] + "_reprojected.shp"
+    print("Reprojecting to", srs)
     ogrCmd = [
-      "ogr2ogr", 
-      workingDir + "/" + reprojected, 
-      workingDir + "/" + f, 
-      "-t_srs", 
-      "EPSG:4326",
-      "-simplify",
-      "10"
+      "ogr2ogr",
+      reprojected,
+      original,
+      "-t_srs", "EPSG:4326"
     ]
     subprocess.call(ogrCmd)
   return reprojected
 
 
+
+def simplifyShapefile(original, outputDir, tolerance):
+  simplified = os.path.join(outputDir, os.path.basename(original))
+  if os.path.exists(simplified):
+    print("Skipping simplification because this file has previously been reprojected.")
+  else:
+    print("Simplifying with tolerance", tolerance)
+    ogrCmd = [
+      "ogr2ogr",
+      simplified,
+      original,
+      "-simplify", tolerance
+    ]
+    subprocess.call(ogrCmd)
+  return simplified
 
 
 
