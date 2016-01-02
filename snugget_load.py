7c60c870c6c71ce7d5b9969792687306fd079ce2
Eldan Goldenberg <eldang@gmail.com>
2016-01-01 20:13:16 -0800

    Now gets section, subsection & filter IDs

--- snugget_load.py
+++ snugget_load.py
@@ -27,14 +27,18 @@ def main():
   
   with psycopg2.connect(host=dbHost, port=dbPort, user=dbUser, password=dbPass, database=dbName) as conn:
     with conn.cursor() as cur:
-      # Find out whats already set up
+      # Find out what's already set up
       sections = readValuesFrom(appName, "snuggetsection", cur)
       subsections = readValuesFrom(appName, "snuggetsubsection", cur)
       oldSnuggets = readValuesFrom(appName, "textsnugget", cur)
       snuggetRefs = readSnuggetCrossRefs(appName, "snugget", cur)
       snuggetColumns = readColumnsFrom(appName, "snugget", cur)
+      print(sections)
+      print(subsections)
+      print(oldSnuggets)
       print(snuggetRefs)
       print(snuggetColumns)
+      print("----------")
       #TODO: should I make snuggetRefs into a dict with snuggetColumns as the keys?
       
       # Then go through the new file, replacing or adding as appropriate
@@ -42,13 +46,42 @@ def main():
         newSnuggets = csv.DictReader(csvFile)
         
         for row in newSnuggets:
-          print(', '.join(row))
+#          print(', '.join(row))
+#          print(', '.join([row[key] for key in row]))
+          # shapefile -> world_snugget.shapefile_filter_id column name; store id we just looked up in that column
+          filterColumn = row["shapefile"] + "_filter_id"
+          # section -> world_snugget.section_id
+          sectionId = getSectionID(appName, row["section"], cur)
+          # subsection -> world_snugget.sub_section_id
+          # in fact... do we want to use subsections at all?
+          # check if a snugget for this data already exists
+          # if we have a lookup value then deal with this value specifically:
+          if row["lookup_value"] is not '':
+            filterID = findFilterID(appName, row["shapefile"], row["lookup_value"], cur)
+            oldSnugget = checkForSnugget(appName, filterColumn, filterID, row["lookup_value"], cur)
+            mode = askUserForMode(row["shapefile"], row["lookup_value"], oldSnugget, [], snuggetFile)
+          # otherwise deal with all the possible values, because blank means "apply to all"
+          else: 
+            filterIDs = findAllFilterIDs(appName, row["shapefile"], cur)
+            oldSnuggets = []
+            for filterID in filterIDs:
+              oldSnugget = checkForSnugget(appName, filterColumn, filterID, row["lookup_value"], cur)
+              if oldSnugget is not None:
+                oldSnuggets.append(oldSnugget)
+            mode = askUserForMode(row["shapefile"], row["lookup_value"], None, oldSnuggets, snuggetFile)
+
+          #   get id for new row in world_snugget -> world_textsnugget.snugget_ptr_id
+          #   heading -> world_textsnugget.heading (null as '')
+          #   intensity -> world_textsnugget.percentage (numeric, null as null)
+          #   if intensity is blank, should we just generate the same row for all intensities?
+          #   image -> world_textsnugget.image
+          #   text -> world_textsnugget.content
 
   
 
 def readValuesFrom(appName, table, cur):
   vals = {}
-  cur.execute("SELECT * FROM " + appName + "_" + table + ";")
+  cur.execute("SELECT * FROM " + appName + "_" + table.lower() + ";")
   for item in cur.fetchall():
     vals[item[1]] = item[0]
   return vals  
@@ -58,7 +91,7 @@ def readValuesFrom(appName, table, cur):
 
 def readColumnsFrom(appName, table, cur):
   cols = []
-  cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = '" + appName + "_" + table + "';")
+  cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = '" + appName + "_" + table.lower() + "';")
   for row in cur.fetchall():
     cols.append(row[0])
   return cols
@@ -72,6 +105,83 @@ def readSnuggetCrossRefs(appName, table, cur):
   for row in cur.fetchall():
     refs.extend(row)
   return refs
+
+
+
+
+def getSectionID(appName, sectionName, cur):
+  cur.execute("SELECT id FROM " + appName + "_snuggetsection WHERE name = '" + sectionName + "';")
+  try:
+    return cur.fetchone()[0]
+  except:
+    cur.execute("INSERT INTO " + appName + "_snuggetsection(name) VALUES(%s);", [sectionName])
+    cur.execute("SELECT id FROM " + appName + "_snuggetsection WHERE name = '" + sectionName + "';")
+    return cur.fetchone()[0]
+  
+  
+
+
+def findFilterID(appName, shapefile, key, cur):
+  cols = readColumnsFrom(appName, shapefile, cur)
+  keyColumn = cols[1]
+  cur.execute("SELECT id FROM " + appName + "_" + shapefile + " WHERE " + keyColumn + "::text = '" + key + "';")
+  return str(cur.fetchone()[0])
+
+    
+    
+def findAllFilterIDs(appName, shapefile, cur):
+  ids = []
+  cur.execute("SELECT id FROM " + appName + "_" + shapefile + ";")
+  for row in cur.fetchall():
+    ids.extend(row)
+  return ids
+  
+
+
+def checkForSnugget(appName, filterColumn, filterID, key, cur):
+  # in each of the following DB queries, an empty return means there's no matching snugget
+  try:
+    cur.execute('SELECT id FROM ' + appName + '_snugget WHERE "' + filterColumn + '" = ' + filterID + ';')
+    snuggetID = str(cur.fetchone()[0])
+    cur.execute("SELECT content FROM " + appName + "_textsnugget WHERE snugget_ptr_id = " + snuggetID + ";")
+    return cur.fetchone()[0]
+  except:
+    return None
+
+  
+  
+  
+def askUserForMode(shapefile, lookup_value, oldSnugget, oldSnuggets, snuggetFile):
+  if oldSnugget is not None:
+    print("In shapefile", shapefile, "there is already a snugget defined for intensity", lookup_value, "with the following text content:")
+    print(oldSnugget)
+  elif oldSnuggets != []:
+    print("In shapefile", shapefile, "there are existing snuggets with the following text content:")
+    duplicates = []
+    for snugget in oldSnuggets:
+      if snugget not in duplicates:
+        print(snugget)
+        duplicates.append(snugget)
+  else: # if both oldSnugget and oldSnuggets were empty, then we don't need to ask the user
+    return "add"
+    
+  print("Please enter one of the following letters to choose how to proceed:")
+  print("A: add a new snugget, in addition to the existing one.")
+  print("R: replace the old snugget with the new value loaded from", snuggetFile)
+  print("Q: quit so you can edit", snuggetFile, "and try again.")
+
+  response = False
+  while response not in ["A", "R", "Q"]:
+    response = input(">> ").upper()
+  
+  if response == "Q":
+    exit(0)
+  elif response == "A":
+    return "add"
+  elif response == "R":
+    return "replace"
+
+
 
 
 
