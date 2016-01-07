2a8461d6e594bdd31fcdd463869c74d7aba76cb3
nein09 <nein09@nein09.com>
2016-01-07 14:08:27 -0800

    Move Hazard Ready to the top

--- world/admin.py
+++ world/admin.py
@@ -3,7 +3,8 @@ from embed_video.admin import AdminVideoMixin
 from solo.admin import SingletonModelAdmin
 ######################################################
 # Replace the next line with generated adminModelImports
-from .models import TextSnugget, EmbedSnugget, SnuggetSection, SnuggetSubSection, Infrastructure, InfrastructureGroup, InfrastructureCategory, RecoveryLevels, Location, SiteSettings
+from .models import TextSnugget, EmbedSnugget, SnuggetSection, SnuggetSubSection, RecoveryLevels, Location, SiteSettings, EQ_GroundShaking_MostLike, Flood_FEMA_DFIRM_2015, MT_groundshaking
+
 ######################################################
 
 admin.site.register(SnuggetSection, admin.ModelAdmin)
@@ -14,14 +15,17 @@ class SnuggetAdmin(admin.ModelAdmin):
 ######################################################
 # Insert generated adminLists here
 ######################################################
+    list_display = ('shortname', 'section', 'sub_section', 'EQ_GroundShaking_MostLike_filter', 'Flood_FEMA_DFIRM_2015_filter', 'MT_groundshaking_filter')
+    list_filter = ('section', 'sub_section', 'EQ_GroundShaking_MostLike_filter', 'Flood_FEMA_DFIRM_2015_filter', 'MT_groundshaking_filter')
+
     fieldsets = (
         (None, {
-            'fields': ('section', 'sub_section'),
+            'fields': ('section', 'sub_section')
         }),
         ('Filters', {
-            'description': 'Choose a filter value this snugget will show up for.',
-            'fields': ((),)
-            }),
+            'description': 'Choose a filter value this snugget will show up for.  It is recommended you only select a value for one filter and leave the rest empty.',
+            'fields': (('EQ_GroundShaking_MostLike_filter', 'Flood_FEMA_DFIRM_2015_filter', 'MT_groundshaking_filter'))
+        })
     )
 
     def shortname(self, obj):
@@ -63,4 +67,9 @@ admin.site.register(Location, SingletonModelAdmin)
 ######################################################
 # Insert generated adminSiteRegistrations here
 ######################################################
+admin.site.register(EQ_GroundShaking_MostLike, GeoNoEditAdmin)
+admin.site.register(Flood_FEMA_DFIRM_2015, GeoNoEditAdmin)
+admin.site.register(MT_groundshaking, GeoNoEditAdmin)
+
+
 
