2e809f7d83fdff47de22ddc6e5fb93a3e4a15c6d
Eldan Goldenberg <eldang@gmail.com>
2016-01-18 11:19:21 -0800

    whitespace cleanup

--- snugget_load.py
+++ snugget_load.py
@@ -29,10 +29,8 @@ def main():
       with open(snuggetFile) as csvFile:
         newSnuggets = csv.DictReader(csvFile)
         for row in newSnuggets:
-          print(any(a != [] for a in row.values()))
           overwriteAll = processRow(appName, snuggetFile, cur, overwriteAll, row)
   print("Snugget load complete.")
-
 
 
 
