b27fc3c5e71a6ac751a942557c0f94376cf9f609
Eldan Goldenberg <eldang@gmail.com>
2016-09-29 16:38:59 -0700

    switched map tiles to thunderforest terrain

--- disasterinfosite/static/js/app.js
+++ disasterinfosite/static/js/app.js
@@ -25,7 +25,7 @@ $( document ).ready(function() {
   }
   map.scrollWheelZoom.disable();
 
-  var osmUrl='//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  var osmUrl='//{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png';
   var osmAttrib='Map data © <a href="//openstreetmap.org">OpenStreetMap</a> contributors';
   var layer = new L.TileLayer(osmUrl, {attribution: osmAttrib}).addTo(map);
   layer.setOpacity(0.6);
