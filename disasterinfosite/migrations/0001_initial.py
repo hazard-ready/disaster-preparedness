37f3a42d2b37ec8b466f54c02dc34caf41a51139
nein09 <nein09@nein09.com>
2016-09-29 13:41:38 -0700

    Add missing import

--- disasterinfosite/migrations/0001_initial.py
+++ disasterinfosite/migrations/0001_initial.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import migrations, models
 import embed_video.fields
 import django.db.models.deletion
