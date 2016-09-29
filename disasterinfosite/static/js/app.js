4a6dfd7df968d61a3eea4e91ea0eafff4a8d521e
nein09 <nein09@nein09.com>
2016-09-29 10:29:32 -0700

    Better error message for iPhone location failures

--- disasterinfosite/static/js/app.js
+++ disasterinfosite/static/js/app.js
@@ -94,7 +94,7 @@ $( document ).ready(function() {
       submitLocation(lat, lng);
     };
     var geoError = function(error) {
-      console.log('Error occurred. Error code: ' + error.code);
+      console.log('Error finding your location: ' + error.message);
       enableForm();
     };
     navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
