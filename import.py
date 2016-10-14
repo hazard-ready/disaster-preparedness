2d1a07e0201fc86840f0c90b1ba5b8a9a79e2abb
Eldan Goldenberg <eldang@gmail.com>
2016-10-14 12:46:24 -0700

    backport bc8caf291ac618c7b0a19585608446327a8a3c2c

--- import.py
+++ import.py
@@ -308,7 +308,8 @@ def modelsGeoFilterGen(stem, keyField):
   text += "        " + stem + "_rating = " + "qs_" + stem + ".values_list('" + keyField.lower() + "', flat=True)\n"
   text += "        for rating in " + stem + "_rating:\n"
   text += "            individualSnugget = Snugget.objects.filter(" + stem + "_filter__" + keyField.lower() + "__exact=rating).select_subclasses()\n"
-  text += "            groupsDict[individualSnugget[0].group.name].extend(individualSnugget)\n\n"
+  text += "            if individualSnugget:\n"
+  text += "                groupsDict[individualSnugget[0].group.name].extend(individualSnugget)\n\n"
   return text
 
 
