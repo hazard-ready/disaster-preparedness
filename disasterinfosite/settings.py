4a21e0729de09f3f3f0492b522aa5e32cba6a6f2
nein09 <nein09@nein09.com>
2017-04-28 15:48:35 -0700

    Add localization directory to settings

--- disasterinfosite/settings.py
+++ disasterinfosite/settings.py
@@ -55,17 +55,13 @@ WSGI_APPLICATION = 'disasterinfosite.wsgi.application'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
-
+LOCALE_PATHS = os.path.join(BASE_DIR, 'locale')
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
 USE_L10N = True
 
+USE_I18N = True
+TIME_ZONE = 'UTC'
 USE_TZ = True
-
 
 TEMPLATES = [
     {
