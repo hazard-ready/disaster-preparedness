3bfeed21ac39e4207e8a32958d221521f78a4652
melinda <melinda@melindaminch.com>
2019-03-27 14:53:06 -0700

    Upgrade npm modules

--- disasterinfosite/webpack.config.js
+++ disasterinfosite/webpack.config.js
@@ -1,6 +1,6 @@
-var path = require("path")
-var webpack = require('webpack')
-var BundleTracker = require('webpack-bundle-tracker')
+var path = require("path");
+var webpack = require("webpack");
+var BundleTracker = require("webpack-bundle-tracker");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var vendorPath = path.join(__dirname, "static/js/vendor/");
@@ -10,71 +10,75 @@ module.exports = {
   resolve: {
     modules: [
       vendorPath,
-      path.join(__dirname, 'node_modules'),
+      path.join(__dirname, "node_modules"),
       path.join(__dirname, "static/js/")
-    ],
+    ]
   },
   entry: {
     vendor: ["leaflet", "jquery", "slick-carousel"],
-    app: './static/js/app'
+    app: "./static/js/app"
   },
 
   output: {
-      path: path.resolve('static/build/'),
-      filename: "[name].js",
+    path: path.resolve("static/build/"),
+    filename: "[name].js"
   },
 
   plugins: [
-    new BundleTracker({filename: './webpack-stats.json'}),
+    new BundleTracker({ filename: "./webpack-stats.json" }),
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery"
     }),
     new webpack.optimize.CommonsChunkPlugin({
       name: "vendor",
-      minChunks: Infinity,
+      minChunks: Infinity
     }),
     new ExtractTextPlugin({
       filename: "[name].css",
       allChunks: true
     }),
     new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: false
-        }
+      compress: {
+        warnings: false
+      }
     })
   ],
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
   module: {
     loaders: [
-    {
-      test: /\.scss$/,
-      use: [
+      {
+        test: /\.scss$/,
+        use: [
           "style-loader", // creates style nodes from JS strings
           "css-loader", // translates CSS into CommonJS
           "sass-loader" // compiles Sass to CSS, using Node Sass by default
-      ]
-    },
-    {
-      test: /\.css(\?v=\d+\.\d+\.\d+)?$/,
-      loader: ExtractTextPlugin.extract({
-        fallback: "style-loader",
-        use: [{
-          loader: "css-loader",
-          query: { minimize: true, sourceMap: true}
-        }]
-      })
-    },
-    {
-      test: /\.(png|gif|jpe?g|svg|ttf|eot|ico)(\?v=\d+\.\d+\.\d+)?$/i,
-      loader: "file-loader?name=[name].[ext],limit=1000"
-    },{
-      test: /\.html(\?v=\d+\.\d+\.\d+)?$/i,
-      loader: 'html-loader'
-    },
-    {
-      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: "url-loader?limit=10000&minetype=application/font-woff"
-    }
-  ]}
-}
+        ]
+      },
+      {
+        test: /\.css(\?v=\d+\.\d+\.\d+)?$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              query: { sourceMap: true }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(png|gif|jpe?g|svg|ttf|eot|ico)(\?v=\d+\.\d+\.\d+)?$/i,
+        loader: "file-loader?name=[name].[ext],limit=1000"
+      },
+      {
+        test: /\.html(\?v=\d+\.\d+\.\d+)?$/i,
+        loader: "html-loader"
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&minetype=application/font-woff"
+      }
+    ]
+  }
+};
