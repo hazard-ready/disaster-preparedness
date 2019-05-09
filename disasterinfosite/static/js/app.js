413e29b083bce0e8d6343cabf92ed37773b86d8d
melinda <melinda@melindaminch.com>
2019-05-09 12:10:20 -0700

    PDX specific stuff for the about page

--- disasterinfosite/static/js/app.js
+++ disasterinfosite/static/js/app.js
@@ -24,6 +24,17 @@ require("../img/gk_headshot.jpg");
 require("../img/linkedin-logo.png");
 require("../img/linkedin-logo@2x.png");
 
+// about page partner images
+require("../img/PBEM-Logo-with-text.jpg");
+require("../img/RDPO-logo.png");
+require("../img/WA-county-OR-logo.jpeg");
+require("../img/clackamas-county.png");
+require("../img/clark county.jpeg");
+require("../img/columbia county.jpeg");
+require("../img/logo-mercy-corps.svg");
+require("../img/multnomah_county_logo.png");
+require("../img/red-cross-logo.png");
+
 require("./users");
 require("slick-carousel");
 
