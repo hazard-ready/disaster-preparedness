ce2826e4cd8e681aa743bac56dcfaef8a30ab899
Eldan Goldenberg <eg@eldang.xyz>
2019-05-28 10:27:43 -0700

    More descriptive variable name

--- disasterinfosite/load.py
+++ disasterinfosite/load.py
@@ -7,7 +7,7 @@ from django.contrib.gis.utils import LayerMapping
 
 # If a raster's height or width is > this threshold, tile it
 # In theory this can be up to INT_MAX * 2, but that runs into memory constraints
-rasterTileMaxDimension = 5000
+rasterTileSize = 5000
 
 
 ######################################################
@@ -20,21 +20,21 @@ rasterTileMaxDimension = 5000
 def tileLoadRaster(model, filename, band=0):
     model.objects.all().delete()
     sourceRaster = GDALRaster(filename, write=True)
-    xTiles = math.ceil(sourceRaster.width / rasterTileMaxDimension)
-    yTiles = math.ceil(sourceRaster.height / rasterTileMaxDimension)
+    xTiles = math.ceil(sourceRaster.width / rasterTileSize)
+    yTiles = math.ceil(sourceRaster.height / rasterTileSize)
     for x in range(0, xTiles):
       if x+1 != xTiles:
-        width = rasterTileMaxDimension
+        width = rasterTileSize
       else:
-        width = sourceRaster.width % rasterTileMaxDimension
-      offsetX = x * rasterTileMaxDimension
+        width = sourceRaster.width % rasterTileSize
+      offsetX = x * rasterTileSize
       originX = sourceRaster.origin.x + offsetX * sourceRaster.scale.x
       for y in range(0, yTiles):
         if y+1 != yTiles:
-          height = rasterTileMaxDimension
+          height = rasterTileSize
         else:
-          height = sourceRaster.height % rasterTileMaxDimension
-        offsetY = y * rasterTileMaxDimension
+          height = sourceRaster.height % rasterTileSize
+        offsetY = y * rasterTileSize
         originY = sourceRaster.origin.y + offsetY * sourceRaster.scale.y
         rasterTile = model(
             rast=GDALRaster({
