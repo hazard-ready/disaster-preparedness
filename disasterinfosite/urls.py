b260f631ea4770d1ead3d19db8670842aeaa5d2b
melinda <melinda@melindaminch.com>
2019-06-20 17:43:34 -0700

    Make login / logout / sign up urls work in translated site version

--- disasterinfosite/urls.py
+++ disasterinfosite/urls.py
@@ -7,14 +7,14 @@ from django.conf import settings
 from disasterinfosite import views
 
 urlpatterns = [
-   url(r'^admin/', admin.site.urls),
-   url(r'^accounts/login/$', views.login_view),
-   url(r'^accounts/logout/$', views.logout_view),
-   url(r'^accounts/create_user/$', views.create_user),
-   url(r'^accounts/update_profile/$', views.update_profile),
-   url(r'^i18n/', include('django.conf.urls.i18n'))
+  url(r'^admin/', admin.site.urls),
+  url(r'^i18n/', include('django.conf.urls.i18n'))
 ]
-# Recommend removing the prefix_default_language argument if you translate/localize this site.
+
+urlpatterns += i18n_patterns(url(r'^accounts/login/$', views.login_view))
+urlpatterns += i18n_patterns(url(r'^accounts/logout/$', views.logout_view))
+urlpatterns += i18n_patterns(url(r'^accounts/create_user/$', views.create_user))
+urlpatterns += i18n_patterns(url(r'^accounts/update_profile/$', views.update_profile))
 urlpatterns += i18n_patterns(url(r'^$', views.app_view, name='index'))
 urlpatterns += i18n_patterns(url(r'^about/$', views.about_view, name='about'))
 urlpatterns += i18n_patterns(url(r'^prepare/$', views.prepare_view, name='prepare'))
