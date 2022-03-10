15d8373d8965a780d5f36b77ecdf926e060417f2
melinda <melinda@melindaminch.com>
2022-03-10 15:44:28 -0800

    Update urls, settings and app config for new versions

--- disasterinfosite/urls.py
+++ disasterinfosite/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib.gis import admin
@@ -7,22 +7,22 @@ from django.conf import settings
 from disasterinfosite import views
 
 urlpatterns = [
-  url(r'^admin/', admin.site.urls),
-  url(r'^i18n/', include('django.conf.urls.i18n')),
+  path('admin/', admin.site.urls),
+  path('i18n/', include('django.conf.urls.i18n')),
+  path('survey/', views.add_survey_code, name="survey"),
   # API urls
-  url(r'^survey/$', views.add_survey_code, name="survey"),
-  url(r'^accounts/login/$', views.login_view, name="login"),
-  url(r'^accounts/logout/$', views.logout_view, name="logout"),
-  url(r'^accounts/create_user/$', views.create_user, name="create_user"),
-  url(r'^accounts/update_profile/$', views.update_profile, name="update_profile"),
-  url(r'^accounts/update_prepare_action/$', views.prepare_action_update, name='prepare_action_update')
+  path('accounts/login/', views.login_view, name="login"),
+  path('accounts/logout/', views.logout_view, name="logout"),
+  path('accounts/create_user/', views.create_user, name="create_user"),
+  path('accounts/update_profile/', views.update_profile, name="update_profile"),
+  path('accounts/update_prepare_action/', views.prepare_action_update, name='prepare_action_update')
 ]
 
 # user-facing URLs
-urlpatterns += i18n_patterns(url(r'^$', views.app_view, name='index'))
-urlpatterns += i18n_patterns(url(r'^about/$', views.about_view, name='about'))
-urlpatterns += i18n_patterns(url(r'^prepare/$', views.prepare_view, name='prepare'))
-urlpatterns += i18n_patterns(url(r'^data/$', views.data_view, name='data'))
+urlpatterns += i18n_patterns(path('', views.app_view, name='index'))
+urlpatterns += i18n_patterns(path('about/', views.about_view, name='about'))
+urlpatterns += i18n_patterns(path('prepare/', views.prepare_view, name='prepare'))
+urlpatterns += i18n_patterns(path('data/', views.data_view, name='data'))
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
